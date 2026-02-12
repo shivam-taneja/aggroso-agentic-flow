94555d44396d3251276f051cb303f7cf81de5f83
Shivam Taneja <shiivamtaneja@gmail.com>
2026-02-12 14:31:53 +0530

    add: cors support to backend
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,30 @@ import { AllExceptionsFilter } from './filters/http-exception.filter';
 import { ValidationExceptionFilter } from './filters/validation-exception.filter';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 
+const VERIFIED_ORIGINS = ['http://localhost:3001'];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (VERIFIED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'), false);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 204,
+  });
 
   app.useGlobalInterceptors(new TransformInterceptor());
 
