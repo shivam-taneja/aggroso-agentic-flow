750249c56da0340f7b6940470f00b15b59380391
Shivam Taneja <shiivamtaneja@gmail.com>
2026-02-12 13:43:44 +0530

    add: gemini module
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GeminiModule } from './modules/gemini/gemini.module';
 
 @Module({
   imports: [
@@ -24,6 +25,8 @@ import { AppService } from './app.service';
         synchronize: true,
       }),
     }),
+
+    GeminiModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/modules/gemini/gemini.service.ts
@@ -0,0 +1,31 @@
+import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class GeminiService {
+  private genAI: GoogleGenerativeAI;
+  private model: GenerativeModel;
+
+  constructor(private config: ConfigService) {
+    const apiKey = this.config.get<string>('GEMINI_API_KEY');
+
+    if (!apiKey) throw new Error('GEMINI_API_KEY missing!');
+
+    this.genAI = new GoogleGenerativeAI(apiKey);
+    this.model = this.genAI.getGenerativeModel({
+      model: 'gemini-3-pro-preview',
+    });
+  }
+
+  async generateText(prompt: string): Promise<string> {
+    try {
+      const result = await this.model.generateContent(prompt);
+      const response = result.response;
+      return response.text();
+    } catch (error) {
+      console.error('Gemini API Error: ', error);
+      throw new InternalServerErrorException('AI Service is unavailable');
+    }
+  }
+}
